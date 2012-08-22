5b861e3bad9923b2c51a2ef81151c078be886255
Deepak Prasanna <deepak@systeminsights.com>
2012-08-22 12:41:42 +0530

    ohm ganeshya namaha

---
+++ app/controllers/Application.scala
@@ -0,0 +1,33 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import play.api.libs.json._
+import play.api.libs.iteratee._
+
+import models._
+
+import akka.actor._
+import akka.util.duration._
+
+object Application extends Controller {
+  
+  def index = Action { implicit request =>
+    Ok(views.html.index())
+  }
+
+  def chatRoom(username: Option[String]) = Action { implicit request =>
+    username.filterNot(_.isEmpty).map { username =>
+      Ok(views.html.chatRoom(username))
+    }.getOrElse {
+      Redirect(routes.Application.index).flashing(
+        "error" -> "please choose a valid username."
+      )
+    }
+  }
+
+
+  }
+  
+}
